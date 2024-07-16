24514a4ea2c053113f9abd77c42e0066e13e98ba
Bhakun laskaw <vishnujena@outlook.com>
2024-07-16 10:23:06 +0530

    Create entrytwo.sh

---
+++ entrytwo.sh
@@ -0,0 +1,26 @@
+pip install setuptools>=70.0.0 --break-system-packages
+
+pip install zipp>=3.19.1 --break-system-packages
+
+rm -fr sessions
+
+rm -fr logs
+
+# remove temp file when previous execution crashed
+rm /tmp/.X99-lock
+
+# set display port and dbus env to avoid hanging (https://github.com/joyzoursky/docker-python-chromedriver)
+export DISPLAY=:99
+export DBUS_SESSION_BUS_ADDRESS=/dev/null
+
+# start virtual display
+Xvfb $DISPLAY -screen 1 1280x800x8 -nolisten tcp &
+
+
+python /app/main.py -cv 126 -v
+
+
+sleep 10
+
+
+python /app/main.py -cv 126 -v
